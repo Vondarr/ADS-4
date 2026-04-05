e5ebca659c645977d9e461406792c78794a40739
Vondarr <vladislavradnov6@gmail.com>
2026-04-05 18:46:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -16,19 +16,33 @@ int countPairs2(int* arr, int len, int value) {
     int target = value - arr[i];
     int left = i + 1;
     int right = len - 1;
+    int first = -1;
     while (left <= right) {
-      int mid = left + (right - left) / 2;
-      if (arr[mid] == target) {
-        k++;
-        break;
-      } else if (arr[mid] < target) {
-        left = mid + 1;
-      } else {
+      int mid = (left + right) / 2;
+      if (arr[mid] >= target) {
+        if (arr[mid] == target) first = mid;
         right = mid - 1;
+      } else {
+        left = mid + 1;
       }
     }
+    if (first != -1) {
+      left = first;
+      right = len - 1;
+      int last = first;
+      while (left <= right) {
+        int mid = (left + right) / 2;
+        if (arr[mid] <= target) {
+          if (arr[mid] == target) last = mid;
+          left = mid + 1;
+        } else {
+          right = mid - 1;
+        }
+      }
+      k += (last - first + 1);
+    }
   }
-  return k;
+  return k / 2;
 }
 int countPairs3(int* arr, int len, int value) {
   int k = 0;
@@ -42,19 +56,19 @@ int countPairs3(int* arr, int len, int value) {
         k += n * (n - 1) / 2;
         break;
       } else {
-        int countLeft = 1;
-        while (i + 1 < j && arr[i] == arr[i + 1]) {
-          countLeft++;
+        int leftVal = arr[i];
+        int rightVal = arr[j];
+        int leftCount = 0;
+        int rightCount = 0;
+        while (i <= j && arr[i] == leftVal) {
+          leftCount++;
           i++;
         }
-        int countRight = 1;
-        while (j - 1 > i && arr[j] == arr[j - 1]) {
-          countRight++;
+        while (i <= j && arr[j] == rightVal) {
+          rightCount++;
           j--;
         }
-        k += countLeft * countRight;
-        i++;
-        j--;
+        k += leftCount * rightCount;
       }
     } else if (sum < value) {
       i++;
